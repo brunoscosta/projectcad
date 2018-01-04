d21538659a9792c47b7e008e9412c2d178727b7e
brunoscosta <34959342+brunoscosta@users.noreply.github.com>
2018-01-04 20:28:24 -0200

    Initial commit

---
+++ src/main/java/com/eletroinfo/projectcad/config/JPAConfig.java
@@ -0,0 +1,85 @@
+package com.eletroinfo.projectcad.config;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.eletroinfo.projectcad.model.Projeto;
+import com.eletroinfo.projectcad.repository.Projetos;
+
+@Configuration
+@EnableJpaRepositories(basePackageClasses = Projetos.class, enableDefaultTransactions = false)
+@EnableTransactionManagement
+public class JPAConfig {
+	
+	@Profile("local")
+	@Bean
+	public DataSource dataSource() {
+		JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
+		dataSourceLookup.setResourceRef(true);
+		return dataSourceLookup.getDataSource("jdbc/projectcadDB");
+	}
+	
+	@Profile("producao")
+	@Bean
+	public DataSource dataSourceProd() throws URISyntaxException {
+		URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+
+	    String username = jdbUri.getUserInfo().split(":")[0];
+	    String password = jdbUri.getUserInfo().split(":")[1];
+	    String port = String.valueOf(jdbUri.getPort());
+	    String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
+	    
+	    BasicDataSource dataSource = new BasicDataSource();
+	    dataSource.setUrl(jdbUrl);
+	    dataSource.setUsername(username);
+	    dataSource.setPassword(password);
+	    dataSource.setInitialSize(10);
+	    return dataSource;
+	}
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(false);
+		adapter.setGenerateDdl(false);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		return adapter;
+	}
+	
+	@Bean
+	public EntityManagerFactory entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setDataSource(dataSource);
+		factory.setJpaVendorAdapter(jpaVendorAdapter);
+		factory.setPackagesToScan(Projeto.class.getPackage().getName());
+		factory.afterPropertiesSet();
+		
+		return factory.getObject();
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		return transactionManager;
+	}
+
+}
---
+++ src/main/resources/db/migration/V01__Criar_tabela_projeto.sql
@@ -0,0 +1,7 @@
+CREATE TABLE projeto (
+	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL,
+	data_inicio DATE,
+	data_fim DATE,
+	status VARCHAR(20)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
